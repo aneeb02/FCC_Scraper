5927cecf0035ae7b8db7f80f1e61f2c8c66296f8
Muhammad Aneeb ur Rehman <67867841+aneeb02@users.noreply.github.com>
2024-10-07 13:09:16 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FCC_Scraper
+
+This program is simple. It takes a query as input. Then it navigates to freeCodeCamp's site, searches for that query, and returns the name and URL of all matching articles in a csv file!
---
+++ scrape.py
@@ -0,0 +1,31 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from urllib.parse import quote_plus
+import time
+
+
+with open("articles.csv", "w") as articles:
+  csv_writer = csv.writer(articles)
+  csv_writer.writerow(['Title', 'URL'])
+  
+
+  driver = webdriver.Chrome()
+  topic = input("What topic would you like to search?")
+  encoded_topic = quote_plus(topic)
+  query = f"https://www.freecodecamp.org/news/search?query={encoded_topic}"
+
+  driver.get(query)
+  time.sleep(3) # wait for site to load completely
+
+  source = driver.page_source
+  soup = BeautifulSoup(source, 'lxml')
+
+  articles = soup.select('div.post-feed')
+  for article in articles:
+    titles = article.select('h2.post-card-title')
+    for title in titles:
+      text = title.select('a')
+      for t in text:
+        csv_writer.writerow([t.text.strip(), t.get('href')])  # write title, url to csv file
